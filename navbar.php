d7892ef884f19bb15ce70e94b96b9c7220646fb6
Gowthamm18 <113981550+Gowthamm18@users.noreply.github.com>
2022-11-20 13:48:34 +0530

    Create navbar.php

---
+++ navbar.php
@@ -0,0 +1,22 @@
+<!-- navbar --> 
+      <nav class="navbar navbar-expand-md navbar-light bg-light">
+      <a class="navbar-brand" href="index.php" style="color : #495057;"><img src="img/logo_small.png" alt="Logo" width="26" style="margin-bottom:4px;"> <b>SPARK FOUNDATION BANK</b></a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="collapsibleNavbar">
+            <ul class="navbar-nav ml-auto">
+              <li class="nav-item">
+                <a class="nav-link" href="index.php" style="color : #495057;"><b>Home</b></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="removeuser.php" style="color : #495057;"><b>List User</b></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="transfermoney.php" style="color : #495057;"><b>Transfer Money</b></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="transactionhistory.php" style="color : #495057;"><b>Transaction History</b></a>
+              </li>
+          </div>
+       </nav>
